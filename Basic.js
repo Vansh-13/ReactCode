7f4173d4ec50bf526918122f6fcc8de9416ab296
Vansh Madaan <madaanvansh68@gmail.com>
2025-02-20 21:41:54 +0530

    done4

--- Basic.js
+++ Basic.js
@@ -9,4 +9,10 @@
 //self closing tag </>
 
 //state
-//ui m update k liye use state
+//ui m update k liye use state..
+
+//VOM --phaletha but ab fiber ka use hota hai instead of vom
+//fiber m list ki performance ko improve kerna bai toh using keys,keys should be "stable, unique and efficient";
+// React would then compare this virtual representation to the actual DOM to find the differences (a process called reconciliation).
+// Fiber is a more advanced architecture introduced in React 16 that improves on VOM by allowing incremental, prioritized, and asynchronous rendering, leading to better performance and smoother user experiences
+// Instead of updating the whole UI in one go, incremental rendering allows React to break the update into smaller chunks or units of work.
