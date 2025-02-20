09bc57d86c79318bfc3347c5cc3e81d01b236b7e
Vansh Madaan <madaanvansh68@gmail.com>
2025-02-20 21:06:11 +0530

    done3

--- 01basic/src/App.jsx
+++ 01basic/src/App.jsx
@@ -1,11 +1,23 @@
-
+import { useState } from "react";
 function App() {
-  
+  let [counter,setcounter]=useState(0);
 
+  // let counter = 15;
   return (
     <>
-    <h1>Hello world</h1>
-    
+      <h1>Hello world</h1>
+      <h2>Counter value: {counter}</h2>
+      <button onClick={()=>{
+       setcounter(
+        counter= counter+1)
+      }}>Add value</button>
+      <br></br>
+      <button onClick={()=>{
+        setcounter(
+          counter=counter-1
+        )
+      }
+      }>remove value</button>
     </>
   )
 }
--- Basic.js
+++ Basic.js
@@ -1,2 +1,12 @@
-// StrictMode is just a helper to highlight issues during development (it doesn't affect the UI).
+// is js library. 
+//jsx javascript XML js m html code likhna.
+// StrictMode is just a helper to highlight issues during development(it doesn't affect the UI).
+//feature.
+//very very page releoad //extreme use of reuable component.
+//very very efficient //single page application.
+//style le liye three ways
+// module css, tailwind css,material css
+//self closing tag </>
 
+//state
+//ui m update k liye use state
